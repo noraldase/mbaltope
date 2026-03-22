1ff53c15bbf1b2bbf15233cee137f6cd1aa5b49c
noraldase <norald711@gmail.com>
2026-03-23 01:42:37 +0700

    Update index-CsZS-EQt.js

--- assets/index-CsZS-EQt.js
+++ assets/index-CsZS-EQt.js
@@ -142,7 +142,7 @@ Error generating stack: `+l.message+`
     }
 
     // Konfigurasi Pakasir
-    const slug = "jayaabadi"; 
+    const slug = "keretakuda"; 
     const order_id = `NEO-${Date.now()}-${playerId}`; 
     const amount = product.price;
     const productName = encodeURIComponent(product.product_name);
