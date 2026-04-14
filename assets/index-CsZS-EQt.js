3ab774b34d65842a75dd9529cb80a70f00c57831
noraldase <norald711@gmail.com>
2026-04-14 10:06:55 +0700

    Update index-CsZS-EQt.js

--- assets/index-CsZS-EQt.js
+++ assets/index-CsZS-EQt.js
@@ -65,7 +65,7 @@ Error generating stack: `+l.message+`
             { product_id: "2", product_name: "350.000.000 Koin", price: 25000, rewards: [{prop_num: 350000000}], product_type: 1 },
             { product_id: "3", product_name: "500.000.000 Koin", price: 32500, rewards: [{prop_num: 500000000}], product_type: 1 },
 			{ product_id: "4", product_name: "1.000.000.000 Koin", price: 65000, rewards: [{prop_num: 1000000000}], product_type: 1 },
-            { product_id: "5", product_name: "2.000.000.000 Koin", price: 13000, rewards: [{prop_num: 2000000000}], product_type: 1 },
+            { product_id: "5", product_name: "2.000.000.000 Koin", price: 130000, rewards: [{prop_num: 2000000000}], product_type: 1 },
             { product_id: "6", product_name: "3.000.000.000 Koin", price: 195000, rewards: [{prop_num: 3000000000}], product_type: 1 },
 			{ product_id: "7", product_name: "5.000.000.000 Koin", price: 325000, rewards: [{prop_num: 5000000000}], product_type: 1 },
             { product_id: "8", product_name: "10.000.000.000 Koin", price: 650000, rewards: [{prop_num: 10000000000}], product_type: 1 },
